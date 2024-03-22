408ed90d0040dd632cfe3fd5a3a1b843d529f60b
molamikedevs <molamikedevs@gmail.com>
2024-03-22 16:48:24 +0400

    new update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Logo from './components/Logo'
 import Form from './components/Form'
 import ParkingList from './components/ParkingList'
 import Stats from './components/Stats'
 
-// const initialItems = [
-// 	{ id: 1, description: 'Passports', quantity: 2, packed: false },
-// 	{ id: 2, description: 'Socks', quantity: 12, packed: false },
-// 	{ id: 3, description: 'Paste', quantity: 1, packed: true },
-// ]
+const initialItems = JSON.parse(localStorage.getItem('items')) || []
 
 export default function App() {
-	const [items, setItems] = useState([])
+	const [items, setItems] = useState(initialItems)
 
 	const handleAddItems = item => {
 		setItems(items => [...items, item])
@@ -30,12 +26,20 @@ export default function App() {
 	}
 
 	const handleClearList = () => {
-		const confirmed = window.confirm(
-			'Are you sure you want to clear list items?'
-		)
+		const confirmed = window.confirm('Are you sure you want to clear the list')
 		if (confirmed) setItems([])
 	}
 
+	useEffect(() => {
+		localStorage.setItem('items', JSON.stringify(items))
+	}, [items])
+
+	useEffect(() => {
+		const storedItems = localStorage.getItem('items')
+		if (storedItems) {
+			setItems(JSON.parse(storedItems))
+		}
+	}, [])
 	return (
 		<div className="app">
 			<Logo />
